4ea87c0ce019274daaf2bde00cc31741226ac9e8
LuanNguyen23T <nguyenhuuhoangluan194@gmail.com>
2025-04-16 12:20:45 +0700

    Delete

--- Course Management/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Course_Management.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
